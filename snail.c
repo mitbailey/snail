f891c7257e80f2fc1cd7f0a7b2b281e62f0a773b
Mit Bailey <mitbailey99@gmail.com>
2023-01-24 20:31:24 -0500

    Initial commit.

---
+++ snail.c
@@ -0,0 +1,15 @@
+/**
+ * @file snail.c
+ * @author Mit Bailey (mitbailey@outlook.com)
+ * @brief Single-actuator Non-ASIC Integrating Latching deformable mirror controller board software.
+ * @version See Git tags for version information.
+ * @date 2023.01.24
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+int main()
+{
+    
+}
